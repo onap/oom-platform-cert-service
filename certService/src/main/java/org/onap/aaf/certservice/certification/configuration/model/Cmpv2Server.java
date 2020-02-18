onap/archived-aaf-certservice

Add configuration reading mechanism for CMP Server
Issue-ID: AAF-997
Signed-off-by: adamwudzinski <adam.wudzinski@nokia.com>
Change-Id: I66ca48b019544f970fb1db18236a093cae28b2aa

certService/src/main/java/org/onap/aaf/certservice/certification/configuration/Cmpv2Server.java
@@ -1,0 +1,71 @@
+/*
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.certification.configuration;
+
+public class Cmpv2Server {
+
+    private Authentication authentication;
+    private String caMode;
+    private String caName;
+    private String issuerDN;
+    private String url;
+
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+
+    public String getCaMode() {
+        return caMode;
+    }
+
+    public void setCaMode(String caMode) {
+        this.caMode = caMode;
+    }
+
+    public String getCaName() {
+        return caName;
+    }
+
+    public void setCaName(String caName) {
+        this.caName = caName;
+    }
+
+    public String getIssuerDN() {
+        return issuerDN;
+    }
+
+    public void setIssuerDN(String issuerDN) {
+        this.issuerDN = issuerDN;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}

onap/oom-platform-cert-service

certService/src/main/java/org/onap/aaf/certservice/certification/configuration/model/Cmpv2Server.java
@@ -1,0 +1,71 @@
+/*
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.certification.configuration.model;
+
+public class Cmpv2Server {
+
+    private Authentication authentication;
+    private CaMode caMode;
+    private String caName;
+    private String issuerDN;
+    private String url;
+
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+
+    public CaMode getCaMode() {
+        return caMode;
+    }
+
+    public void setCaMode(CaMode caMode) {
+        this.caMode = caMode;
+    }
+
+    public String getCaName() {
+        return caName;
+    }
+
+    public void setCaName(String caName) {
+        this.caName = caName;
+    }
+
+    public String getIssuerDN() {
+        return issuerDN;
+    }
+
+    public void setIssuerDN(String issuerDN) {
+        this.issuerDN = issuerDN;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}