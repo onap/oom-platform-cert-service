7244c908edd4aa392e65d820c90845323918b47b
Tomasz Golabek <tomasz.golabek@nokia.com>
2020-02-21 15:49:34 +0100

    Added actuator for refreshing configuration
    Cloud-config dependency is provided
    Event listener introduced to handle refresh call
    Refresh endpoint exposed
    Fixed docker-compose to start locally and use build images
    Update repositories for docker image and install vim on it
    
    Issue-ID: AAF-997
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>
    Change-Id: Iee005518c901dad7730c5f48c410ec89850f1425

--- certService/src/main/java/org/onap/aaf/certservice/certification/configuration/CmpServersConfig.java
+++ certService/src/main/java/org/onap/aaf/certservice/certification/configuration/CmpServersConfig.java
@@ -21,29 +21,50 @@
 package org.onap.aaf.certservice.certification.configuration;
 
 import org.onap.aaf.certservice.certification.configuration.model.Cmpv2Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.context.event.EventListener;
 
+@RefreshScope
 @Configuration
 public class CmpServersConfig {
+
     private static final String CMP_SERVERS_CONFIG_FILENAME = "cmpServers.json";
 
-    @Autowired
-    private CmpServersConfigLoader cmpServersConfigLoader;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CmpServersConfig.class);
+    private static final String REFRESHING_CONFIGURATION = "Refreshing configuration";
+
     @Value("${app.config.path}")
     private String configPath;
+
+    private CmpServersConfigLoader cmpServersConfigLoader;
     private List<Cmpv2Server> cmpServers;
+
+    @Autowired
+    public CmpServersConfig(CmpServersConfigLoader cmpServersConfigLoader) {
+        this.cmpServersConfigLoader = cmpServersConfigLoader;
+    }
 
     @PostConstruct
     void loadConfiguration() {
         String configFilePath = configPath + File.separator + CMP_SERVERS_CONFIG_FILENAME;
         this.cmpServers = Collections.unmodifiableList(cmpServersConfigLoader.load(configFilePath));
+    }
+
+    @EventListener
+    public void onRefreshScope(final RefreshScopeRefreshedEvent event) {
+        LOGGER.info(REFRESHING_CONFIGURATION);
+        loadConfiguration();
     }
 
     public List<Cmpv2Server> getCmpServers() {
