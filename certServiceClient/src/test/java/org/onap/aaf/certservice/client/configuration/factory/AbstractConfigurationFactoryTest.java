onap/archived-aaf-certservice

Add validation for OUTPUT_TYPE parameter
Issue-ID: AAF-1152

Signed-off-by: pwielebs <piotr.wielebski@nokia.com>
Change-Id: Iec46b8a7b8e1818b62877f5272368636dc1929a0

certServiceClient/src/test/java/org/onap/aaf/certservice/client/configuration/factory/AbstractConfigurationFactoryTest.java
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.mock;
 
 public class AbstractConfigurationFactoryTest {
 
-    private AbstractConfigurationFactory cut = mock(AbstractConfigurationFactory.class, Mockito.CALLS_REAL_METHODS);
+    private final AbstractConfigurationFactory cut = mock(AbstractConfigurationFactory.class, Mockito.CALLS_REAL_METHODS);
 
     @ParameterizedTest
     @ValueSource(strings = {"/var/log", "/", "/var/log/", "/second_var", "/second-var"})
@@ -76,5 +76,17 @@ public class AbstractConfigurationFactoryTest {
     @ValueSource(strings = {"https://example.com", "http://example.com", "example.com:8080", "0.0.0.0", "@#$%.com"})
     public void shouldRejectInvalidCommonName(String commonName) {
         assertThat(cut.isCommonNameValid(commonName)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"JKS", "PC12", "PEM"})
+    public void shouldAcceptValidOutputType(String outputType) {
+        assertThat(cut.isOutputTypeValid(outputType)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"jks", "pc12", "pem", "", "pass", "!@$#pp"})
+    public void shouldRejectInvalidOutputType(String outputType) {
+        assertThat(cut.isOutputTypeValid(outputType)).isFalse();
     }
 }

onap/oom-platform-cert-service

certServiceClient/src/test/java/org/onap/aaf/certservice/client/configuration/factory/AbstractConfigurationFactoryTest.java
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.mock;
 
 public class AbstractConfigurationFactoryTest {
 
-    private AbstractConfigurationFactory cut = mock(AbstractConfigurationFactory.class, Mockito.CALLS_REAL_METHODS);
+    private final AbstractConfigurationFactory cut = mock(AbstractConfigurationFactory.class, Mockito.CALLS_REAL_METHODS);
 
     @ParameterizedTest
     @ValueSource(strings = {"/var/log", "/", "/var/log/", "/second_var", "/second-var"})
@@ -76,5 +76,17 @@ public class AbstractConfigurationFactoryTest {
     @ValueSource(strings = {"https://example.com", "http://example.com", "example.com:8080", "0.0.0.0", "@#$%.com"})
     public void shouldRejectInvalidCommonName(String commonName) {
         assertThat(cut.isCommonNameValid(commonName)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"JKS", "P12", "PEM"})
+    public void shouldAcceptValidOutputType(String outputType) {
+        assertThat(cut.isOutputTypeValid(outputType)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"jks", "p12", "pem", "", "pass", "!@$#pp"})
+    public void shouldRejectInvalidOutputType(String outputType) {
+        assertThat(cut.isOutputTypeValid(outputType)).isFalse();
     }
 }