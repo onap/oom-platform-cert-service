onap/archived-aaf-certservice

Add PEM artifacts creation with unit tests
Issue-ID: AAF-1152
Change-Id: I95afd62330f3111f916507d628d142262ff951cc
Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/conversion/PKCS12ArtifactsCreator.java
@@ -21,18 +21,27 @@ package org.onap.aaf.certservice.client.certification.conversion;
 
 import java.security.PrivateKey;
 import java.util.List;
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
 import org.onap.aaf.certservice.client.certification.exception.PemToPKCS12ConverterException;
+import org.onap.aaf.certservice.client.certification.writer.CertFileWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PKCS12ArtifactsCreator implements ArtifactsCreator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PKCS12ArtifactsCreator.class);
     private static final String CERTIFICATE_ALIAS = "certificate";
     private static final String TRUSTED_CERTIFICATE_ALIAS = "trusted-certificate-";
     private static final int PASSWORD_LENGTH = 24;
+    private static final String KEYSTORE_P12 = "keystore.p12";
+    private static final String KEYSTORE_PASS = "keystore.pass";
+    private static final String TRUSTSTORE_P12 = "truststore.p12";
+    private static final String TRUSTSTORE_PASS = "truststore.pass";
     private final RandomPasswordGenerator generator;
     private final PemToPKCS12Converter converter;
-    private final PKCS12FilesCreator creator;
+    private final CertFileWriter creator;
 
-    public PKCS12ArtifactsCreator(PKCS12FilesCreator creator, RandomPasswordGenerator generator,
+    public PKCS12ArtifactsCreator(CertFileWriter creator, RandomPasswordGenerator generator,
                                   PemToPKCS12Converter converter) {
         this.generator = generator;
         this.converter = converter;
@@ -40,22 +49,33 @@ public class PKCS12ArtifactsCreator implements ArtifactsCreator {
     }
 
     @Override
-    public void create(List<String> keystoreData, List<String> truststoreData, PrivateKey privateKey) throws PemToPKCS12ConverterException {
+    public void create(List<String> keystoreData, List<String> truststoreData, PrivateKey privateKey)
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         createKeystore(keystoreData,privateKey);
         createTruststore(truststoreData);
     }
 
     private void createKeystore(List<String> data, PrivateKey privateKey)
-        throws PemToPKCS12ConverterException {
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         Password password = generator.generate(PASSWORD_LENGTH);
-        creator.saveKeystoreData(converter.convertKeystore(data, password, CERTIFICATE_ALIAS, privateKey),
-            password.getCurrentPassword());
+
+        LOGGER.debug("Attempt to create PKCS12 keystore files and saving data. File names: {}, {}", KEYSTORE_P12, KEYSTORE_PASS);
+
+        creator.saveData(converter.convertKeystore(data, password, CERTIFICATE_ALIAS, privateKey), KEYSTORE_P12);
+        creator.saveData(getPasswordAsBytes(password), KEYSTORE_PASS);
     }
 
     private void createTruststore(List<String> data)
-        throws PemToPKCS12ConverterException {
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         Password password = generator.generate(PASSWORD_LENGTH);
-        creator.saveTruststoreData(converter.convertTruststore(data, password, TRUSTED_CERTIFICATE_ALIAS),
-            password.getCurrentPassword());
+
+        LOGGER.debug("Attempt to create PKCS12 truststore files and saving data. File names: {}, {}", TRUSTSTORE_P12, TRUSTSTORE_PASS);
+
+        creator.saveData(converter.convertTruststore(data, password, TRUSTED_CERTIFICATE_ALIAS), TRUSTSTORE_P12);
+        creator.saveData(getPasswordAsBytes(password), TRUSTSTORE_PASS);
+    }
+
+    private byte[] getPasswordAsBytes(Password password) {
+        return password.getCurrentPassword().getBytes();
     }
 }

certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/writer/CertFileWriter.java
@@ -1,0 +1,47 @@
+/*============LICENSE_START=======================================================
+ * aaf-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.client.certification.writer;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CertFileWriter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CertFileWriter.class);
+    private final String destPath;
+
+    public CertFileWriter(String destPath) {
+        this.destPath = destPath;
+    }
+
+public void saveData(byte[] data, String filename) throws CertFileWriterException {
+        LOGGER.debug("Attempt to save file {} in path {}", filename, destPath);
+        try (FileOutputStream fos = new FileOutputStream(destPath + filename)) {
+            fos.write(data);
+            } catch (IOException e) {
+            LOGGER.error("File creation failed, exception message: {}", e.getMessage());
+            throw new CertFileWriterException(e);
+        }
+    }
+}

certServiceClient/src/test/java/org/onap/aaf/certservice/client/certification/writer/CertFileWriterTest.java
@@ -1,0 +1,82 @@
+/*============LICENSE_START=======================================================
+ * aaf-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.client.certification.writer;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
+
+class CertFileWriterTest {
+
+    private static final String RESOURCES_PATH = "src/test/resources";
+    private static final String OUTPUT_PATH = RESOURCES_PATH + "/generatedFiles/";
+    private static final String TRUSTSTORE_P12 = "truststore.p12";
+    private static final String ERROR_MESSAGE = "java.io.FileNotFoundException: src/test/resources/generatedFiles/thisPathDoesNotExist/truststore.p12 (No such file or directory)";
+
+    private File outputDirectory = new File(OUTPUT_PATH);
+
+    @BeforeEach
+    void createDirectory() {
+        outputDirectory.mkdir();
+    }
+
+    @AfterEach
+    void cleanUpFiles() {
+        List.of(outputDirectory.listFiles()).forEach(f -> f.delete());
+        outputDirectory.delete();
+    }
+
+    @Test
+    void certFileWriterShouldCreateFilesWithDataInGivenLocation()
+        throws IOException, CertFileWriterException {
+        // given
+        final byte[] data = new byte[]{-128, 1, 2, 3, 127};
+        File truststore = new File(OUTPUT_PATH + TRUSTSTORE_P12);
+        CertFileWriter certFileWriter = new CertFileWriter(OUTPUT_PATH);
+
+        // when
+        certFileWriter.saveData(data, TRUSTSTORE_P12);
+
+        // then
+        assertTrue(truststore.exists());
+        assertArrayEquals(data, Files.readAllBytes(Path.of(OUTPUT_PATH + TRUSTSTORE_P12)));
+    }
+
+    @Test
+    void certFileWriterShouldThrowPemToPKCS12ConverterExceptionWhenOutputDirectoryDoesNotExist() {
+        // given
+        final byte[] data = new byte[]{-128, 1, 2, 3, 0};
+        CertFileWriter certFileWriter = new CertFileWriter(OUTPUT_PATH + "thisPathDoesNotExist/");
+
+        // when then
+        assertThatThrownBy(() -> certFileWriter.saveData(data, TRUSTSTORE_P12))
+            .isInstanceOf(CertFileWriterException.class).hasMessage(ERROR_MESSAGE);
+    }
+}

onap/oom-platform-cert-service

certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/conversion/PKCS12ArtifactsCreator.java
@@ -21,41 +21,61 @@ package org.onap.aaf.certservice.client.certification.conversion;
 
 import java.security.PrivateKey;
 import java.util.List;
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
 import org.onap.aaf.certservice.client.certification.exception.PemToPKCS12ConverterException;
+import org.onap.aaf.certservice.client.certification.writer.CertFileWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PKCS12ArtifactsCreator implements ArtifactsCreator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PKCS12ArtifactsCreator.class);
     private static final String CERTIFICATE_ALIAS = "certificate";
     private static final String TRUSTED_CERTIFICATE_ALIAS = "trusted-certificate-";
     private static final int PASSWORD_LENGTH = 24;
+    private static final String KEYSTORE_P12 = "keystore.p12";
+    private static final String KEYSTORE_PASS = "keystore.pass";
+    private static final String TRUSTSTORE_P12 = "truststore.p12";
+    private static final String TRUSTSTORE_PASS = "truststore.pass";
     private final RandomPasswordGenerator generator;
     private final PemToPKCS12Converter converter;
-    private final PKCS12FilesCreator creator;
+    private final CertFileWriter writer;
 
-    public PKCS12ArtifactsCreator(PKCS12FilesCreator creator, RandomPasswordGenerator generator,
+    public PKCS12ArtifactsCreator(CertFileWriter writer, RandomPasswordGenerator generator,
                                   PemToPKCS12Converter converter) {
         this.generator = generator;
         this.converter = converter;
-        this.creator = creator;
+        this.writer = writer;
     }
 
     @Override
-    public void create(List<String> keystoreData, List<String> truststoreData, PrivateKey privateKey) throws PemToPKCS12ConverterException {
+    public void create(List<String> keystoreData, List<String> truststoreData, PrivateKey privateKey)
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         createKeystore(keystoreData,privateKey);
         createTruststore(truststoreData);
     }
 
     private void createKeystore(List<String> data, PrivateKey privateKey)
-        throws PemToPKCS12ConverterException {
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         Password password = generator.generate(PASSWORD_LENGTH);
-        creator.saveKeystoreData(converter.convertKeystore(data, password, CERTIFICATE_ALIAS, privateKey),
-            password.getCurrentPassword());
+
+        LOGGER.debug("Attempt to create PKCS12 keystore files and saving data. File names: {}, {}", KEYSTORE_P12, KEYSTORE_PASS);
+
+        writer.saveData(converter.convertKeystore(data, password, CERTIFICATE_ALIAS, privateKey), KEYSTORE_P12);
+        writer.saveData(getPasswordAsBytes(password), KEYSTORE_PASS);
     }
 
     private void createTruststore(List<String> data)
-        throws PemToPKCS12ConverterException {
+        throws PemToPKCS12ConverterException, CertFileWriterException {
         Password password = generator.generate(PASSWORD_LENGTH);
-        creator.saveTruststoreData(converter.convertTruststore(data, password, TRUSTED_CERTIFICATE_ALIAS),
-            password.getCurrentPassword());
+
+        LOGGER.debug("Attempt to create PKCS12 truststore files and saving data. File names: {}, {}", TRUSTSTORE_P12, TRUSTSTORE_PASS);
+
+        writer.saveData(converter.convertTruststore(data, password, TRUSTED_CERTIFICATE_ALIAS), TRUSTSTORE_P12);
+        writer.saveData(getPasswordAsBytes(password), TRUSTSTORE_PASS);
+    }
+
+    private byte[] getPasswordAsBytes(Password password) {
+        return password.getCurrentPassword().getBytes();
     }
 }

certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/writer/CertFileWriter.java
@@ -1,0 +1,48 @@
+/*============LICENSE_START=======================================================
+ * aaf-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.client.certification.writer;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.nio.file.Path;
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CertFileWriter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CertFileWriter.class);
+    private final String destPath;
+
+    public CertFileWriter(String destPath) {
+        this.destPath = destPath;
+    }
+
+public void saveData(byte[] data, String filename) throws CertFileWriterException {
+        LOGGER.debug("Attempt to save file {} in path {}", filename, destPath);
+        try (FileOutputStream outputStream = new FileOutputStream(Path.of(destPath, filename).toString())) {
+            outputStream.write(data);
+            } catch (IOException e) {
+            LOGGER.error("File creation failed, exception message: {}", e.getMessage());
+            throw new CertFileWriterException(e);
+        }
+    }
+}

certServiceClient/src/test/java/org/onap/aaf/certservice/client/certification/writer/CertFileWriterTest.java
@@ -1,0 +1,81 @@
+/*============LICENSE_START=======================================================
+ * aaf-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.client.certification.writer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.onap.aaf.certservice.client.certification.exception.CertFileWriterException;
+
+class CertFileWriterTest {
+
+    private static final String RESOURCES_PATH = "src/test/resources";
+    private static final String OUTPUT_PATH = RESOURCES_PATH + "/generatedFiles/";
+    private static final String TRUSTSTORE_P12 = "truststore.p12";
+    private static final String ERROR_MESSAGE = "java.io.FileNotFoundException: src/test/resources/generatedFiles/thisPathDoesNotExist/truststore.p12 (No such file or directory)";
+
+    private File outputDirectory = new File(OUTPUT_PATH);
+
+    @BeforeEach
+    void createDirectory() {
+        outputDirectory.mkdir();
+    }
+
+    @AfterEach
+    void cleanUpFiles() {
+        List.of(outputDirectory.listFiles()).forEach(f -> f.delete());
+        outputDirectory.delete();
+    }
+
+    @Test
+    void certFileWriterShouldCreateFilesWithDataInGivenLocation()
+        throws IOException, CertFileWriterException {
+        // given
+        final byte[] data = new byte[]{-128, 1, 2, 3, 127};
+        File truststore = new File(OUTPUT_PATH + TRUSTSTORE_P12);
+        CertFileWriter certFileWriter = new CertFileWriter(OUTPUT_PATH);
+
+        // when
+        certFileWriter.saveData(data, TRUSTSTORE_P12);
+
+        // then
+        assertThat(truststore.exists()).isTrue();
+        assertThat(Files.readAllBytes(Path.of(OUTPUT_PATH + TRUSTSTORE_P12))).isEqualTo(data);
+    }
+
+    @Test
+    void certFileWriterShouldThrowPemToPKCS12ConverterExceptionWhenOutputDirectoryDoesNotExist() {
+        // given
+        final byte[] data = new byte[]{-128, 1, 2, 3, 0};
+        CertFileWriter certFileWriter = new CertFileWriter(OUTPUT_PATH + "thisPathDoesNotExist/");
+
+        // when then
+        assertThatThrownBy(() -> certFileWriter.saveData(data, TRUSTSTORE_P12))
+            .isInstanceOf(CertFileWriterException.class).hasMessage(ERROR_MESSAGE);
+    }
+}