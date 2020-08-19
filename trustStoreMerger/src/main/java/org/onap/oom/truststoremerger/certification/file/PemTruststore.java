5509d0b1aee9e05c374b657804e965525abd394a
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2020-08-19 10:38:43 +0200

    Add Certification merge logic
    Issue-ID: DCAEGEN2-2253
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: I4157ce71373b41d9f7ee03b76d407ec7a2b17ec3

--- trustStoreMerger/src/main/java/org/onap/oom/truststoremerger/certification/file/PemTruststore.java
+++ trustStoreMerger/src/main/java/org/onap/oom/truststoremerger/certification/file/PemTruststore.java
@@ -20,18 +20,132 @@
 package org.onap.oom.truststoremerger.certification.file;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.Security;
 import java.security.cert.Certificate;
-import java.util.Collections;
+import java.security.cert.CertificateFactory;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
+import org.bouncycastle.util.io.pem.PemObjectGenerator;
+import org.bouncycastle.util.io.pem.PemWriter;
+import org.onap.oom.truststoremerger.api.ExitableException;
+import org.onap.oom.truststoremerger.certification.entry.CertificateWithAlias;
+import org.onap.oom.truststoremerger.certification.entry.CertificateWithAliasFactory;
+import org.onap.oom.truststoremerger.certification.file.exception.MissingTruststoreException;
+import org.onap.oom.truststoremerger.certification.file.exception.TruststoreDataOperationException;
+import org.onap.oom.truststoremerger.certification.file.exception.WriteTruststoreFileException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PemTruststore extends TruststoreFile {
+
+    private static final String X_509_INSTANCE = "X.509";
+    private static final String BOUNCY_CASTLE_INSTANCE = "BC";
+    private static final boolean APPEND_TO_FILE = true;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(P12Truststore.class);
+    private final CertificateWithAliasFactory factory = new CertificateWithAliasFactory();
+    private List<CertificateWithAlias> certificatesToBeSaved = new ArrayList<>();
 
     public PemTruststore(File truststoreFile) {
         super(truststoreFile);
     }
 
     @Override
-    public List<Certificate> getCertificates() {
-        return Collections.emptyList();
+    public List<CertificateWithAlias> getCertificates() throws ExitableException {
+        LOGGER.debug("Reading certificates from file: {}", this.getTruststoreFile().getPath());
+        List<Certificate> rawCertificates = getRawCertificates(this.getTruststoreFile());
+        return wrapCertificates(rawCertificates);
     }
+
+    @Override
+    public void addCertificate(List<CertificateWithAlias> certificates) {
+        LOGGER.debug("Adding certificates to file: {}", this.getTruststoreFile().getPath());
+        certificatesToBeSaved.addAll(certificates);
+    }
+
+    @Override
+    public void saveFile() throws ExitableException {
+        LOGGER.debug("Saving file: {} ", this.getTruststoreFile().getPath());
+        String certificatesAsString = getCertificatesAsString(certificatesToBeSaved);
+        appendToFile(certificatesAsString);
+    }
+
+    private List<Certificate> getRawCertificates(File file)
+        throws MissingTruststoreException, TruststoreDataOperationException {
+        List<Certificate> extractedCertificate = extractCertificatesFromFile(file);
+        if (extractedCertificate.isEmpty()) {
+            throw new MissingTruststoreException("PEM file not contains any certificate");
+        }
+        return extractedCertificate;
+    }
+
+    private List<Certificate> extractCertificatesFromFile(File file) throws TruststoreDataOperationException {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            Security.addProvider(new BouncyCastleProvider());
+            CertificateFactory factory = CertificateFactory.getInstance(X_509_INSTANCE, BOUNCY_CASTLE_INSTANCE);
+            return new ArrayList<>(factory.generateCertificates(inputStream));
+        } catch (Exception e) {
+            LOGGER.error("Cannot read certificates from file: {}", file.getPath());
+            throw new TruststoreDataOperationException(e);
+        }
+    }
+
+    private List<CertificateWithAlias> wrapCertificates(List<Certificate> rawCertificates) {
+        return rawCertificates.stream()
+            .map(factory::createPemCertificate)
+            .collect(Collectors.toList());
+    }
+
+    private void appendToFile(String certificatesAsString) throws WriteTruststoreFileException {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(this.getTruststoreFile(), APPEND_TO_FILE);
+            fileOutputStream.write(certificatesAsString.getBytes());
+        } catch (Exception e) {
+            LOGGER.error("Cannot write certificates to file");
+            throw new WriteTruststoreFileException(e);
+        }
+    }
+
+    private String getCertificatesAsString(List<CertificateWithAlias> certificates) throws ExitableException {
+        StringWriter sw = new StringWriter();
+        List<PemObjectGenerator> generators = transformToPemGenerators(certificates);
+        try (PemWriter pemWriter = new PemWriter(sw)) {
+            for (PemObjectGenerator generator : generators) {
+                pemWriter.writeObject(generator);
+            }
+        } catch (IOException e) {
+            LOGGER.error("Cannot convert certificates to PEM format");
+            throw new TruststoreDataOperationException(e);
+        }
+        return sw.toString();
+    }
+
+    private List<PemObjectGenerator> transformToPemGenerators(List<CertificateWithAlias> certificates)
+        throws ExitableException {
+        List<PemObjectGenerator> generators = new ArrayList<>();
+        for (CertificateWithAlias certificatesWithAlias : certificates) {
+            PemObjectGenerator generator = createPemGenerator(certificatesWithAlias.getCertificate());
+            generators.add(generator);
+        }
+        return generators;
+    }
+
+    private JcaMiscPEMGenerator createPemGenerator(Certificate certificate)
+        throws TruststoreDataOperationException {
+        try {
+            return new JcaMiscPEMGenerator(certificate);
+        } catch (IOException e) {
+            LOGGER.error("Cannot convert Certificate Object to PemGenerator Object");
+            throw new TruststoreDataOperationException(e);
+        }
+    }
+
+
 }
---
+++ trustStoreMerger/src/test/java/org/onap/oom/truststoremerger/certification/file/CertificatesTestFileManager.java
@@ -0,0 +1,128 @@
+/*============LICENSE_START=======================================================
+ * oom-truststore-merger
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.truststoremerger.certification.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.onap.oom.truststoremerger.certification.file.exception.KeystoreInstanceException;
+import org.onap.oom.truststoremerger.certification.file.exception.LoadTruststoreException;
+import org.onap.oom.truststoremerger.certification.file.provider.CertificateStoreController;
+import org.onap.oom.truststoremerger.certification.file.provider.CertificateStoreControllerFactory;
+
+public class CertificatesTestFileManager {
+
+    public static final String SAMPLE_P12_TRUSTSTORE_FILE_PATH = "src/test/resources/truststore-p12.p12";
+    public static final String SAMPLE_P12_TRUSTSTORE_PASSWORD = "88y9v5D8H3SG6bZWRVHDfOAo";
+    public static final String TMP_P12_TRUSTSTORE_FILE_PATH = "src/test/resources/tmp-truststore-p12.p12";
+
+    public static final String SAMPLE_P12_KEYSTORE_FILE_PATH = "src/test/resources/keystore.p12";
+    public static final String SAMPLE_P12_KEYSTORE_PASSWORD = "Foh49MJNYI7S_pEzE9gvUDSu";
+
+    public static final String SAMPLE_JKS_TRUSTSTORE_FILE_PATH = "src/test/resources/truststore-jks.jks";
+    public static final String SAMPLE_JKS_TRUSTSTORE_UNIQUE_ALIAS_FILE_PATH = "src/test/resources/truststore-jks-uniq.jks";
+    public static final String SAMPLE_JKS_TRUSTSTORE_PASSWORD = "EOyuFbuYDyq_EhpboM72RHua";
+    public static final String TMP_JKS_TRUSTSTORE_FILE_PATH = "src/test/resources/tmp-truststore-jks.jks";
+
+    public static final String SAMPLE_PEM_TRUSTSTORE_FILE_PATH = "src/test/resources/truststore.pem";
+    public static final String EMPTY_PEM_TRUSTSTORE_FILE_PATH = "src/test/resources/empty-truststore.pem";
+    public static final String TMP_PEM_TRUSTSTORE_FILE_PATH = "src/test/resources/tmp-truststore.pem";
+
+    private static final CertificateStoreControllerFactory certificateStoreControllerFactory = new CertificateStoreControllerFactory();
+
+    public static P12Truststore getSampleP12Truststore() throws LoadTruststoreException, KeystoreInstanceException {
+        return createP12TruststoreInstance(SAMPLE_P12_TRUSTSTORE_FILE_PATH, SAMPLE_P12_TRUSTSTORE_PASSWORD);
+    }
+
+    public static P12Truststore getSampleP12Keystore() throws LoadTruststoreException, KeystoreInstanceException {
+        return createP12TruststoreInstance(SAMPLE_P12_KEYSTORE_FILE_PATH, SAMPLE_P12_KEYSTORE_PASSWORD);
+    }
+
+    public static P12Truststore createTmpP12TruststoreFile()
+        throws IOException, LoadTruststoreException, KeystoreInstanceException {
+        copyFile(SAMPLE_P12_TRUSTSTORE_FILE_PATH, TMP_P12_TRUSTSTORE_FILE_PATH);
+        return createP12TruststoreInstance(TMP_P12_TRUSTSTORE_FILE_PATH, SAMPLE_P12_TRUSTSTORE_PASSWORD);
+    }
+
+    public static P12Truststore getTmpP12TruststoreFile() throws LoadTruststoreException, KeystoreInstanceException {
+        return createP12TruststoreInstance(TMP_P12_TRUSTSTORE_FILE_PATH, SAMPLE_P12_TRUSTSTORE_PASSWORD);
+    }
+
+    private static P12Truststore createP12TruststoreInstance(String filePath, String password)
+        throws LoadTruststoreException, KeystoreInstanceException {
+        File certFile = getFile(filePath);
+        CertificateStoreController storeController = certificateStoreControllerFactory
+            .createLoadedPkcs12CertificateStoreController(certFile, password);
+        return new P12Truststore(certFile, password, storeController);
+    }
+
+    public static PemTruststore getSamplePemTruststoreFile() {
+        return new PemTruststore(getFile(SAMPLE_PEM_TRUSTSTORE_FILE_PATH));
+    }
+
+    public static PemTruststore createEmptyTmpPemTruststoreFile() throws IOException {
+        copyFile(EMPTY_PEM_TRUSTSTORE_FILE_PATH, TMP_PEM_TRUSTSTORE_FILE_PATH);
+        return new PemTruststore(getFile(TMP_PEM_TRUSTSTORE_FILE_PATH));
+    }
+
+    public static PemTruststore getEmptyTmpPemTruststoreFile() {
+        return new PemTruststore(getFile(TMP_PEM_TRUSTSTORE_FILE_PATH));
+    }
+
+    public static JksTruststore getSampleJksTruststoreFile() throws LoadTruststoreException, KeystoreInstanceException {
+        return createJKSTruststoreInstance(SAMPLE_JKS_TRUSTSTORE_FILE_PATH, SAMPLE_JKS_TRUSTSTORE_PASSWORD);
+    }
+
+    public static JksTruststore getSampleJksTruststoreFileWithUniqueAlias()
+        throws LoadTruststoreException, KeystoreInstanceException {
+        return createJKSTruststoreInstance(SAMPLE_JKS_TRUSTSTORE_UNIQUE_ALIAS_FILE_PATH,
+            SAMPLE_JKS_TRUSTSTORE_PASSWORD);
+    }
+
+    public static JksTruststore createTmpJksTruststoreFileWithUniqAlias()
+        throws IOException, LoadTruststoreException, KeystoreInstanceException {
+        copyFile(SAMPLE_JKS_TRUSTSTORE_UNIQUE_ALIAS_FILE_PATH, TMP_JKS_TRUSTSTORE_FILE_PATH);
+        return createJKSTruststoreInstance(TMP_JKS_TRUSTSTORE_FILE_PATH, SAMPLE_JKS_TRUSTSTORE_PASSWORD);
+    }
+
+    public static void removeTemporaryFiles() throws IOException {
+        Files.deleteIfExists(Paths.get(TMP_PEM_TRUSTSTORE_FILE_PATH));
+        Files.deleteIfExists(Paths.get(TMP_JKS_TRUSTSTORE_FILE_PATH));
+        Files.deleteIfExists(Paths.get(TMP_P12_TRUSTSTORE_FILE_PATH));
+    }
+
+    private static JksTruststore createJKSTruststoreInstance(String filePath, String password)
+        throws LoadTruststoreException, KeystoreInstanceException {
+        File certFile = getFile(filePath);
+        CertificateStoreController storeController = certificateStoreControllerFactory
+            .createLoadedJksCertificateStoreController(certFile, password);
+        return new JksTruststore(certFile, password, storeController);
+    }
+
+    private static void copyFile(String sourcePath, String destPath) throws IOException {
+        Files.copy(Paths.get(sourcePath), Paths.get(destPath), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static File getFile(String path) {
+        return new File(path);
+    }
+}
