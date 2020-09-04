8879a2dc5cd76ed72121fe4eeb7f0777d4dd1fcb
Piotr Marcinkiewicz <piotr.marcinkiewicz@nokia.com>
2020-09-04 13:03:29 +0200

    Refactor and revide package structure
    - move classes between packages
    - rename controllers classes
    
    Issue-ID: DCAEGEN2-2253
    Signed-off-by: Piotr Marcinkiewicz <piotr.marcinkiewicz@nokia.com>
    Change-Id: I897ee5dae72de020077dacd814bb9cde6db11866

---
+++ trustStoreMerger/src/main/java/org/onap/oom/truststoremerger/certification/model/Truststore.java
@@ -0,0 +1,62 @@
+/*============LICENSE_START=======================================================
+ * oom-truststore-merger
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.truststoremerger.certification.model;
+
+import java.io.File;
+import java.util.List;
+import org.onap.oom.truststoremerger.api.ExitableException;
+import org.onap.oom.truststoremerger.certification.exception.CreateBackupException;
+import org.onap.oom.truststoremerger.certification.model.certificate.CertificateWithAlias;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Truststore {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Truststore.class);
+
+    private final TruststoreEntity truststoreEntity;
+
+    public Truststore(TruststoreEntity truststoreEntity) {
+        this.truststoreEntity = truststoreEntity;
+    }
+
+    public List<CertificateWithAlias> getCertificates() throws ExitableException {
+        LOGGER.debug("Attempt to read certificates from file: {}", truststoreEntity.getStoreFile().getPath());
+        return truststoreEntity.getNotEmptyCertificateList();
+    }
+
+    public void addCertificate(List<CertificateWithAlias> certificates) throws ExitableException {
+        LOGGER.debug("Attempt to add certificates for saving to file");
+        truststoreEntity.addCertificates(certificates);
+    }
+
+    public void saveFile() throws ExitableException {
+        LOGGER.debug("Attempt to save file: {}", truststoreEntity.getStoreFile().getPath());
+        truststoreEntity.saveFile();
+    }
+
+    public void createBackup() throws CreateBackupException {
+        truststoreEntity.createBackup();
+    }
+
+    File getStoreFile() {
+        return truststoreEntity.getStoreFile();
+    }
+}
