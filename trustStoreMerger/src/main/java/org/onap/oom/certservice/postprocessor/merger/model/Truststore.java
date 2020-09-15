a49275dbab946926fd6f1ae77d9bed2b6c1a3b63
Jan Malkiewicz <jan.malkiewicz@nokia.com>
2020-09-15 10:07:54 +0200

    [OOM-CMPv2] Renamed pakage o.o.oom.truststoremerger -> o.o.oom.certservice.postprocessor
    Issue-ID: DCAEGEN2-2253
    Signed-off-by: Jan Malkiewicz <jan.malkiewicz@nokia.com>
    Change-Id: Ie360bcfd07288c82231ef92fdf0afff5dee1641d

---
+++ trustStoreMerger/src/main/java/org/onap/oom/certservice/postprocessor/merger/model/Truststore.java
@@ -0,0 +1,49 @@
+/*============LICENSE_START=======================================================
+ * oom-truststore-merger
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.certservice.postprocessor.merger.model;
+
+import java.io.File;
+import java.util.List;
+import org.onap.oom.certservice.postprocessor.merger.exception.CreateBackupException;
+import org.onap.oom.certservice.postprocessor.api.ExitableException;
+import org.onap.oom.certservice.postprocessor.common.FileTools;
+import org.onap.oom.certservice.postprocessor.merger.model.certificate.CertificateWithAlias;
+
+public abstract class Truststore {
+
+    final File storeFile;
+
+    private final FileTools fileTools;
+
+    public Truststore(File storeFile, FileTools fileTools) {
+        this.storeFile = storeFile;
+        this.fileTools = fileTools;
+    }
+
+    public void createBackup() throws CreateBackupException {
+        fileTools.createBackup(storeFile);
+    }
+
+    public abstract List<CertificateWithAlias> getCertificates() throws ExitableException;
+
+    public abstract void addCertificates(List<CertificateWithAlias> certificates) throws ExitableException;
+
+    public abstract void saveFile() throws ExitableException;
+}
