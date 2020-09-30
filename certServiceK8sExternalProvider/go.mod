fc1070312fe8dccafd5418b0c2713b8eaa9ce12d
Jan Malkiewicz <jan.malkiewicz@nokia.com>
2020-09-30 11:29:19 +0200

    [OOM-K8S-CERT-EXTERNAL-PROVIDER] Add project
    This project is a GOlang implementation of an external provider for kubernetes cert-manager.
    External provider will use OOM CertService as backend signing CA.
    
    Issue-ID: OOM-2559
    Signed-off-by: Jan Malkiewicz <jan.malkiewicz@nokia.com>
    Change-Id: Ide31ca64c29763c9d4e9bdf58d8f50ffddcaaf5d

---
+++ certServiceK8sExternalProvider/go.mod
@@ -0,0 +1,8 @@
+module oom-certservice/k8s-external-provider
+
+go 1.15
+
+require (
+	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
+	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
+)
---
+++ certServiceK8sExternalProvider/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("   ***  Hello World Golang App  ***")
+	fmt.Println()
+
+	//resp, err := grequests.Get("http://onap-wro-dashboard.dyn.nesc.nokia.net/live", nil)
+	//// You can modify the request by passing an optional RequestOptions struct
+	//
+	//if err != nil {
+	//	log.Fatalln("Unable to make request: ", err)
+	//}
+	//
+	//fmt.Println("Checking status of the app Dashboard-NG:")
+	//fmt.Println(resp.String())
+
+}
