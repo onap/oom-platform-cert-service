7bff90473e5ab6e788070778ad986b6528ef9ea3
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-10-16 16:06:04 +0200

    [OOM-K8S-CERT-EXTERNAL-PROVIDER] Add unit tests to cmpv2_provisioner
    Add tests with input and output .pem files
    Tests should be adjusted to future implementation when possible
    
    Issue-ID: OOM-2559
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>
    Change-Id: I39952c2cb0bd35bfd8337b22ca6e95c86509b4f7

--- certServiceK8sExternalProvider/src/cmpv2provisioner/cmpv2_provisioner_test.go
+++ certServiceK8sExternalProvider/src/cmpv2provisioner/cmpv2_provisioner_test.go
@@ -21,10 +21,159 @@
 package cmpv2provisioner
 
 import (
+	"bytes"
+	"context"
+	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
+	"io/ioutil"
+	apimach "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"log"
+	"onap.org/oom-certservice/k8s-external-provider/src/cmpv2api"
 	"testing"
+	"time"
 )
 
-func TestSignCertificate(t *testing.T) {
+func Test_shouldCreateCorrectCertServiceCA(t *testing.T){
+	issuer, key := createIssuerAndKey("cmpv2-issuer", "issuer/url", "onapwro")
+	provisioner, err := New(&issuer, key)
 
-	t.Logf("Dummy GO test --> Everything is OK <--.")
+	if(err != nil) {
+		t.Log("Could not create provisioner.", err)
+		t.FailNow()
+	}
+
+	if(string(provisioner.key) != string(key)){
+		t.Log("Unexpected provisioner key.")
+		t.FailNow()
+	}
+
+	if(provisioner.name != issuer.Name){
+		t.Log("Unexpected provisioner name.")
+		t.FailNow()
+	}
+
+	if(provisioner.url != issuer.Spec.URL){
+		t.Log("Unexpected provisioner url.")
+		t.FailNow()
+	}
+}
+
+func Test_shouldSuccessfullyLoadPreviouslyStoredProvisioner(t *testing.T){
+	issuer, key := createIssuerAndKey("cmpv2-issuer", "issuer/url", "onapwro")
+	provisioner, err := New(&issuer, key)
+
+	if(err != nil) {
+		t.Log("Could not create provisioner.", err)
+		t.FailNow()
+	}
+
+	issuerNamespaceName := createIssuerNamespaceName("onap", "cmpv2")
+
+	Store(issuerNamespaceName, provisioner)
+	provisioner, ok := Load(issuerNamespaceName)
+
+	if(!ok) {
+		t.Log("Provisioner could not be loaded.")
+		t.FailNow()
+	}
+
+	if(string(provisioner.key) != string(key)){
+		t.Log("Unexpected provisioner key.")
+		t.FailNow()
+	}
+
+	if(provisioner.name != issuer.Name){
+		t.Log("Unexpected provisioner name.")
+		t.FailNow()
+	}
+
+	if(provisioner.url != issuer.Spec.URL){
+		t.Log("Unexpected provisioner url.")
+		t.FailNow()
+	}
+}
+
+func Test_shouldReturnCorrectSignedPemsWhenParametersAreCorrect(t *testing.T) {
+	issuer, key := createIssuerAndKey("cmpv2-issuer", "issuer/url", "onapwro")
+
+	provisioner, err := New(&issuer, key)
+	issuerNamespaceName := createIssuerNamespaceName("onap", "cmpv2")
+	Store(issuerNamespaceName, provisioner)
+
+	provisioner, ok := Load(issuerNamespaceName)
+
+	if(!ok){
+		t.Log("Provisioner could not be loaded")
+		t.FailNow()
+	}
+
+	ctx := context.Background()
+	request := createCertificateRequest()
+
+	signedPEM, trustedCAs, err := provisioner.Sign(ctx, request)
+
+	if(err != nil){
+		t.Log(err)
+		t.FailNow()
+	}
+
+	if(!areSlicesEqual(signedPEM, readFile("expected_signed.pem"))){
+		t.Log("Signed pem is different than expected.")
+		t.FailNow()
+	}
+	if(!areSlicesEqual(trustedCAs, readFile("expected_trusted.pem"))){
+		t.Log("Trusted CAs pem is different than expected.")
+		t.FailNow()
+	}
+}
+
+func createIssuerNamespaceName(namespace string, name string) types.NamespacedName{
+	return types.NamespacedName{
+		Namespace: namespace,
+		Name:      name,
+	}
+}
+
+func createIssuerAndKey(name string, url string, key string) (cmpv2api.CMPv2Issuer, []byte) {
+	issuer := cmpv2api.CMPv2Issuer{}
+	issuer.Name = name
+	issuer.Spec.URL = url
+	return issuer, []byte(key)
+}
+
+func readFile(filename string) []byte{
+	certRequest, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return certRequest
+}
+
+func createCertificateRequest() *cmapi.CertificateRequest {
+	duration := new(apimach.Duration)
+	d, _ := time.ParseDuration("1h")
+	duration.Duration = d
+
+	request := new(cmapi.CertificateRequest)
+	request.Spec.Duration = duration
+
+	request.Spec.IssuerRef.Name = "cmpv2-issuer"
+	request.Spec.IssuerRef.Kind = "CMPv2Issuer"
+	request.Spec.IssuerRef.Group = "certmanager.onap.org"
+
+	request.Spec.Request = readFile("test_certificate_request.pem")
+
+	request.Spec.IsCA = true
+
+	cond := new(cmapi.CertificateRequestCondition)
+	cond.Type = "Ready"
+	request.Status.Conditions = []cmapi.CertificateRequestCondition{*cond}
+
+	request.Status.Certificate = readFile("test_certificate.pem")
+
+	return request
+}
+
+func areSlicesEqual(slice1 []byte, slice2 []byte) bool{
+	return bytes.Compare(slice1, slice2) == 0
 }
