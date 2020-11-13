3a5ee1d1e166d06fe6c45d4cf3dd9a616924f8dd
kjaniak <kornel.janiak@nokia.com>
2020-11-13 15:03:17 +0100

    [OOM cert-service-client] Add validation of email, ip and domain name
    Added SanMapper to distinguish types of SANS.
    ExitableException changed to RuntimeException.
    Introcudced intermediate object ValidatedSan.
    
    Issue-ID: OOM-2559
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>
    Change-Id: I060de9869ab9fd737a474f683a251abd8431d224

---
+++ certServiceClient/src/main/java/org/onap/oom/certservice/client/configuration/model/ValidatedSan.java
@@ -0,0 +1,73 @@
+/*
+ * ============LICENSE_START=======================================================
+ * oom-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.certservice.client.configuration.model;
+
+import java.util.Objects;
+
+public class ValidatedSan {
+
+    private final String san;
+    private final int typeOfSan;
+
+    public ValidatedSan(String san, int typeOfSan) {
+        this.san = san;
+        this.typeOfSan = typeOfSan;
+    }
+
+    public String getSan() {
+        return san;
+    }
+
+    public int getTypeOfSan() {
+        return typeOfSan;
+    }
+
+    public String toString() {
+        return "{san: " + san + ", typeOfSan: " + getReadableSanType(typeOfSan) + '}';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ValidatedSan san1 = (ValidatedSan) o;
+        return typeOfSan == san1.typeOfSan &&
+            Objects.equals(san, san1.san);
+    }
+
+    public int hashCode() {
+        return Objects.hash(san, typeOfSan);
+    }
+
+    private String getReadableSanType(int typeOfSan) {
+        String readableSanType = "undefined";
+        switch (typeOfSan) {
+            case 1: readableSanType = "rfc822Name"; break;
+            case 2: readableSanType = "dNSName"; break;
+            case 6: readableSanType = "uniformResourceIdentifier"; break;
+            case 7: readableSanType = "iPAddress"; break;
+        }
+        return readableSanType;
+    }
+}
---
+++ certServiceClient/src/main/java/org/onap/oom/certservice/client/configuration/validation/SanMapper.java
@@ -0,0 +1,47 @@
+/*
+ * ============LICENSE_START=======================================================
+ * oom-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.certservice.client.configuration.validation;
+
+import static org.onap.oom.certservice.client.configuration.validation.BasicValidationFunctions.isDomainNameValid;
+import static org.onap.oom.certservice.client.configuration.validation.BasicValidationFunctions.isEmailAddressValid;
+import static org.onap.oom.certservice.client.configuration.validation.BasicValidationFunctions.isIpAddressValid;
+
+import java.util.function.Function;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.onap.oom.certservice.client.configuration.exception.CsrConfigurationException;
+import org.onap.oom.certservice.client.configuration.model.ValidatedSan;
+
+public class SanMapper {
+
+    public Function<String, ValidatedSan> create() {
+        return (san) -> {
+            if (isEmailAddressValid(san)) {
+                return new ValidatedSan(san, GeneralName.rfc822Name);
+            } else if (isIpAddressValid(san)) {
+                return new ValidatedSan(san, GeneralName.iPAddress);
+            } else if (isDomainNameValid(san)) {
+                return new ValidatedSan(san, GeneralName.dNSName);
+            } else {
+                throw new CsrConfigurationException("San :" + san + " does not match any requirements");
+            }
+        };
+    }
+}
