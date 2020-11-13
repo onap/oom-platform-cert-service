b554818a3c235cda3318893973d213d8696feb6a
kjaniak <kornel.janiak@nokia.com>
2020-11-13 15:03:17 +0100

    [OOM cert-service-client] Add validation of email, ip and domain name
    Added SanMapper to distinguish types of SANS.
    ExitableException changed to RuntimeException.
    Introcudced intermediate object San.
    
    Issue-ID: OOM-2559
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>
    Change-Id: I060de9869ab9fd737a474f683a251abd8431d224

---
+++ certServiceClient/src/main/java/org/onap/oom/certservice/client/configuration/factory/SanMapper.java
@@ -0,0 +1,48 @@
+/*
+ * ============LICENSE_START=======================================================
+ * oom-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.certservice.client.configuration.factory;
+
+import static org.onap.oom.certservice.client.configuration.validation.UriValidator.isValidUri;
+import static org.onap.oom.certservice.client.configuration.validation.csr.CsrEnvsValueValidators.isDomainNameValid;
+import static org.onap.oom.certservice.client.configuration.validation.csr.CsrEnvsValueValidators.isEmailAddressValid;
+import static org.onap.oom.certservice.client.configuration.validation.csr.CsrEnvsValueValidators.isIpAddressValid;
+
+import java.util.function.Function;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.onap.oom.certservice.client.configuration.exception.CsrConfigurationException;
+import org.onap.oom.certservice.client.configuration.model.San;
+
+public class SanMapper implements Function<String, San> {
+
+    public San apply(String san) {
+        if (isEmailAddressValid(san)) {
+            return new San(san, GeneralName.rfc822Name);
+        } else if (isIpAddressValid(san)) {
+            return new San(san, GeneralName.iPAddress);
+        } else if (isDomainNameValid(san)) {
+            return new San(san, GeneralName.dNSName);
+        } else if (isValidUri(san)) {
+            return new San(san, GeneralName.uniformResourceIdentifier);
+        } else {
+            throw new CsrConfigurationException("San :" + san + " does not match any requirements");
+        }
+    }
+}
---
+++ certServiceClient/src/main/java/org/onap/oom/certservice/client/configuration/validation/csr/CsrEnvsValueValidators.java
@@ -0,0 +1,71 @@
+/*
+ * ============LICENSE_START=======================================================
+ * oom-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.oom.certservice.client.configuration.validation.csr;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import org.apache.commons.validator.routines.DomainValidator;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+public final class CsrEnvsValueValidators {
+    private CsrEnvsValueValidators() {}
+
+    private static final String SPECIAL_CHAR_PRESENCE_REGEX = "[~#@*$+%!()?/{}<>\\|_^]";
+    private static final String PORT_POSTFIX_REGEX = ":[0-9]{1,5}";
+    private static final String IPV4_ADDRESS_REGEX = "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}";
+    private static final String HTTP_HTTPS_SCHEME_REGEX = "[h][t][t][p][:][/][/]|[h][t][t][p][s][:][/][/]";
+    private static final boolean ALLOW_LOCAL_DOMAINS = true;
+
+    public static boolean isSpecialCharPresent(String stringToCheck) {
+        return Pattern.compile(SPECIAL_CHAR_PRESENCE_REGEX).matcher(stringToCheck).find();
+    }
+
+    public static boolean isPortNumberPresent(String stringToCheck) {
+        return Pattern.compile(PORT_POSTFIX_REGEX).matcher(stringToCheck).find();
+    }
+
+    public static boolean isIpAddressPresent(String stringToCheck) {
+        return Pattern.compile(IPV4_ADDRESS_REGEX).matcher(stringToCheck).find();
+    }
+
+    public static boolean isHttpProtocolsPresent(String stringToCheck) {
+        return Pattern.compile(HTTP_HTTPS_SCHEME_REGEX).matcher(stringToCheck).find();
+    }
+
+    public static boolean isCountryValid(String country) {
+        return Arrays.asList(Locale.getISOCountries()).contains(country);
+    }
+
+    public static boolean isEmailAddressValid(String address) {
+        return EmailValidator.getInstance().isValid(address);
+    }
+
+    public static boolean isIpAddressValid(String address) {
+        return InetAddressValidator.getInstance().isValid(address);
+    }
+
+    public static boolean isDomainNameValid(String domain) {
+        return DomainValidator.getInstance(ALLOW_LOCAL_DOMAINS).isValid(domain);
+    }
+
+}
