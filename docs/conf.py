1d0252fbdfcb4437253f32a24739e132bac9da97
thmsdt <thomas.kulik@telekom.de>
2022-06-08 09:05:53 +0200

    fix config for doc (jakarta)
    Issue-ID: DOC-782
    
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>
    Change-Id: I35327afc12b0276df0100d0c1d3a7f90c3a0e73e

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,20 @@
 from docs_conf.conf import *
-
-branch = 'latest'
+ 
+branch = 'jakarta'
 master_doc = 'index'
-
+ 
 linkcheck_ignore = [
     'http://localhost',
+    'http://ejbca:8080/ejbca/publicweb/cmp/cmpRA',
 ]
-
-extensions = [
-    'sphinxcontrib.openapi',
+ 
+exclude_patterns = [
+    '.tox'
 ]
+ 
 intersphinx_mapping = {}
-
+ 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
-
-
+ 
 def setup(app):
     app.add_css_file("css/ribbon.css")
