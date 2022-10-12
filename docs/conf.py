8f4877bb9408ad0ca85572bfc2d2f97737a1d997
thmsdt <thomas.kulik@telekom.de>
2022-10-12 10:51:44 +0200

    add linkcheck_ignore (jakarta)
    Issue-ID: DOC-782
    
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>
    Change-Id: Id240ba300b36b5eed4ab4322bf33d873582d15de

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ intersphinx_mapping = {}
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
-
 def setup(app):
     app.add_css_file("css/ribbon.css")
+
+linkcheck_ignore = [r'https://download.primekey.com/docs/EJBCA-Enterprise/6_14_0/CMP.html']
+linkcheck_ignore = [r'http://ejbca:8080/ejbca/publicweb/cmp/cmpRA']
